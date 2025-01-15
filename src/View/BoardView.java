60d5b43fbcae46e0b9b3df6d04e04ee4744ec989
AqilRahmat <aqilr161@gmail.com>
2025-01-15 23:22:23 +0800

    Image Display + Changed squares to JPanel

--- src/View/BoardView.java
+++ src/View/BoardView.java
@@ -15,15 +15,16 @@ public class BoardView {
         frame = new JFrame("Kwazam Chess - TT5L GD");
         frame.setLayout(new GridLayout(9,5, -1, -1));
         frame.setSize(500, 600);
+        frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        displayMenus();
+        displayInitialBoard();
+
         frame.setVisible(true);
     }
 
     public void displayMenus() {
-        //logo image
-        JLabel logoLabel = new JLabel();
-        logoLabel.setIcon(new ImageIcon("Img/logochess.png"));
-
         //Save Button
         JButton save = new JButton("Save");
 
@@ -37,7 +38,7 @@ public class BoardView {
         frame.add(save);
         frame.add(load);
         frame.add(newgame);
-        frame.add(logoLabel);
+        frame.add(new JLabel());
         frame.add(new JLabel());
     }
 
@@ -46,10 +47,44 @@ public class BoardView {
 
         for(String[] row : boardArray) {
             for(String cell : row) {
+                JPanel panel = new JPanel();
                 JLabel label = new JLabel(cell, SwingConstants.CENTER);
-                label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-                frame.add(label);
+                panel.add(label, BorderLayout.CENTER);
+                panel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                panel.setPreferredSize(new Dimension(100, 100));
+                String addimg = label.getText();
+                getPieceImages(addimg.trim(), label);
+                frame.add(panel);
             }
+        }
+    }
+
+    public void getPieceImages(String cell, JLabel label) {
+        ImageIcon image = new ImageIcon();
+        switch(cell) {
+            case "R":
+                image = new ImageIcon("src/Img/Ram.png");
+                label.setIcon(image);
+                break;
+            case "B":
+                image = new ImageIcon("src/Img/Biz.png");
+                label.setIcon(image);
+                break;
+            case "T":
+                image = new ImageIcon("src/Img/Tor.png");
+                label.setIcon(image);
+                break;
+            case "S":
+                image = new ImageIcon("src/Img/Sau.png");
+                label.setIcon(image);
+                break;
+            case "X":
+                image = new ImageIcon("src/Img/Xor.png");
+                label.setIcon(image);
+                break;
+            default:
+                label.setIcon(null);
+                break;
         }
     }
 }
--- src/View/Start.java
+++ src/View/Start.java
@@ -6,8 +6,5 @@ public class Start {
     public static void main(String[] args) {
         Board board = new Board();
         BoardView boardView = new BoardView(board);
-
-        boardView.displayMenus();
-        boardView.displayInitialBoard();
     }
 }
